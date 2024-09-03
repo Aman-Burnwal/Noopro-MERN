3d0d888937bb642866afdc6d70d4e60c02171c0d
Aman Burnwal <amankr06534@gmail.com>
2024-09-03 13:37:19 +0530

    controllers for backend

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,6 +1,11 @@
+import Profile from "../model/Profile";
+
 const OTP = require("../model/Otp");
 const User = require("../model/User")
 const otpGenrator = require("otp-generator");
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+require("dotenv").config()
 
 // send OTP
 
@@ -71,15 +76,165 @@ exports.signUP = async (req, res) => {
 
     try {
         
-        
+        const {firstName, lastName, email, password, confirmPassword, accountType, contactNumber, otp} = req.body;
+
+
+        // validate data
+
+        if(!firstName || !lastName || !email || !password || !confirmPassword || !otp) return res.status(403).json({
+            success: false,
+            message: "Sab input fields sahi se to bharo nahi to aage nhi jane dunga mai 👿"
+        })
+
+
+        // match the password
+
+        if(password != confirmPassword) return res.status(400).json({
+            success: false,
+            message: "dono password match nahi kar rha h ji"
+        });
+
+
+        // check kar lo kahi user register to nhi h
+
+        const existingUser = await User.find({email});
+
+        if(existingUser) return res.status(400).json({
+            success: false,
+            message: "Tu to pahle se register h phir kahe ko phir se register kar rha h "
+        })
+
+
+        // find most recent otp stored for the user
+
+        const recentOTP = await OTP.find({email}).sort({createdAt: -1}).limit(1);
+        console.log("recentOTP", recentOTP);
+
+        // validateOTP
+
+        if(recentOTP.length == 0) return res.status(400).json({
+            success: false,
+            message: "OTP to mila hi nhi :("
+        })
+
+        if(otp != recentOTP.otp) res.status(400).json({
+            success: false,
+            message: "OTP to match hi nhi huwa ji"
+        })
+
+        // hash password 
+
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        const profileDetails = await Profile.create({gender:null, dateOfBirth: null, about: null, contactNumber: null});
+
+        // create entry in Db
+        const user = new User({
+            firstName, lastName, email, contactNumber, accountType, password:hashPassword,
+            additonDetail: profileDetails._id,
+            
+            iamge:`https://ui-avatars.com/api/?name=${firstName}+${lastName}`
+        })
+
+        return res.status(200).json({
+            success:true,
+            message: "Bhai mubarak ho tum to register ho gaye :)"
+        })
 
 
     } catch (error) {
-        
+        console.error("error in registration", error);
+        res.status(500).json({
+            success: false,
+            message: "Kuchh to gadbad hai daya jo user register nhi ho paya "
+        })
     }
 }
 
 
 // Login
 
+module.Login = async(req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        // validate the data 
+
+        if(!email || !password) return res.status(403).json({
+            success: false,
+            message: "email aur password dono field bharna h ji"
+        })
+
+        // check kahi user register hi to nhi hai
+
+        const user = await User.findOne({email}).populate("additonDetail");
+
+        if(!user) return res.status(401).json({
+            success: false,
+            message: "Register ho ke login kar nhi to utha utha ke patkunga chori karne ke iljam me"
+        })
+
+
+        // generate jWT token, after matching the token
+
+        if(await bcrypt.compare(password, user.password)) {
+            const payload = {
+                email: user.email,
+                id: user._id,
+                accountType: user.accountType
+            }
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                expiresIn: "2h"
+            })
+
+            user.token = token;
+            user.password = undefined;
+
+            // create cookie and send response
+            const options = {
+                exprires: new Date(Date.now() + 3 * 24 * 3600 * 1000),
+                httpOnly: true,
+            }
+            res.cookie("token", token, options).staus(200).json({
+                success: true,
+                token, 
+                user, 
+                message: "Arrey shabash login ho gaya tu to........🙌"
+            })
+        }else {
+            res.staus(401).json({
+                success: false,
+                message: "Password agar sahi deta na to abhi tu login kar jata"
+            })
+        }
+
+        
+
+    } catch (error) {
+        console.error("error in Login", error);
+
+        res.status(500).json({
+            success: false, 
+            message: "Ab tu login bhi nhi kar pa rha h yarr\n hadd hoti hai bachkane ki 😒"
+        })
+    }
+}
+
 // changePassword
+
+module.ChangePassword = async(req, res) => {
+
+    try {
+        
+        // get data from body
+        // get oldPassword, newPassword, confirmPassword
+
+        // validation
+
+        // update pwd in DB
+        // send mail - password updated
+        // return response
+    } catch (error) {
+        
+    }
+}
