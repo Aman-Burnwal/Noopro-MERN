e93d464a75940dbec856bd63b5ad1b97e26f06ec
Aman Burnwal <amankr06534@gmail.com>
2024-09-17 23:15:12 +0530

    front end setup

--- noopro/tailwind.config.js
+++ noopro/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
@@ -132,4 +132,5 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
+
