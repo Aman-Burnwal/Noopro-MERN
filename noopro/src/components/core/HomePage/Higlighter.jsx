62fb2e3ceebb9309ba76ecb43b42fe552a2ea210
Aman Burnwal <amankr06534@gmail.com>
2024-09-18 20:36:49 +0530

    highlight text

---
+++ noopro/src/components/core/HomePage/Higlighter.jsx
@@ -0,0 +1,11 @@
+
+
+const Higlighter = ({text}) => {
+  return (
+    <div className="text-transparent bg-clip-text bg-gradient-to-b from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] w-fit">
+       {" " + text + " "}
+    </div>
+  )
+}
+
+export default Higlighter
