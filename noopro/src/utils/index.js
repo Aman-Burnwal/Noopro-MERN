7f4e3007ee27ec890bd165d0dc79238b451c8a5e
Aman Burnwal <amankr06534@gmail.com>
2024-09-19 13:50:30 +0530

    import export are now centralized for files

---
+++ noopro/src/utils/index.js
@@ -0,0 +1,7 @@
+import HiglightBtn from "../components/core/HomePage/HighlightBtn";
+import Higlighter from "../components/core/HomePage/Highlighter";
+import Home from "../pages/Home";
+import Preview from "../components/core/HomePage/Preview";
+
+
+export {HiglightBtn, Higlighter, Home, Preview }
