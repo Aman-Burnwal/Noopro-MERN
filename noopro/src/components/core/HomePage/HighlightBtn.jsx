462c652c591622883863985d780f5b0384a5f7a2
Aman Burnwal <amankr06534@gmail.com>
2024-09-19 13:48:01 +0530

    renamed file

--- noopro/src/components/core/HomePage/HighlightBtn.jsx
+++ noopro/src/components/core/HomePage/HighlightBtn.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const HiglightBtn = ({active , link, text}) => {
   return (
-    <Link link to= {link}>
+    <Link to= {link}>
       <button className={`group px-6 py-3  rounded-lg  font-medium text-center
          hover:scale-95 transition-all duration-200 drop-shadow-[2px_1.5px_rgba(255,255,255,0.25)]
           ${active ? " bg-yellow-50 text-richblack-900" : "bg-richblack-700 text-white"} `}>
---
+++ noopro/src/components/core/HomePage/Highlighter.jsx
@@ -0,0 +1,11 @@
+
+
+const Higlighter = ({text}) => {
+  return (
+    <span className="text-transparent bg-clip-text bg-gradient-to-b from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] w-fit">
+       { text }
+    </span>
+  )
+}
+
+export default Higlighter
