fb14a95430d8860413ca1bf5c2d31a843ffc9406
Aman Burnwal <amankr06534@gmail.com>
2024-10-16 23:41:26 +0530

    formated

--- noopro/src/pages/common/Navigation.jsx
+++ noopro/src/pages/common/Navigation.jsx
@@ -34,52 +34,54 @@ const Navigation = () => {
 
 
                     {/* navigation bar */}
-                    <ul className="flex flex-row gap-3 text-richblue-5 ">
-                        {
-                            NavbarLinks.map((item) => {
-                                return (
-                                    <li
-                                        className={`${location.pathname === item.path ? " text-yellow-5" : ""}`}
-                                        key={item.title}>
-                                        {item.title == "Catalog" ?
-                                            (
-                                                <div className="relative">
-                                                <div className="flex flex-row gap-[1px] items-center  cursor-pointer">
-                                                    {item.title}
-                                                    <IoIosArrowDown />
-                                                </div>
+                    <nav>
+                        <ul className="flex flex-row gap-3 text-richblue-5 ">
 
-                                                {/* <ul className="bg-white w-[290px] hidden group-hover:flex -ml-20 mt-[18px] rounded-lg text-richblack-900 absolute  flex-col gap-5 p-8">
-                                                <li className="hover:bg-richblack-50 rounded-lg pl-4 py-2 pr-16 text-left">Python </li>
-                                                <li className="hover:bg-richblack-50 rounded-lg pl-4 py-2 pr-16 text-left">Web devlopment</li>
-                                                <li className="hover:bg-richblack-50 rounded-lg pl-4 py-2 pr-16 text-left">Linux</li>
-                                                </ul> */}
+                            {
+                                NavbarLinks.map((item) => {
+                                    return (
+                                        <li
+                                            className={`${location.pathname === item.path ? " text-yellow-5" : ""}`}
+                                            key={item.title}>
+                                            {item.title == "Catalog" ?
+                                                (
+                                                    <div className="relative">
+                                                        <div className="flex flex-row gap-[1px] items-center  cursor-pointer">
+                                                            {item.title}
+                                                            <IoIosArrowDown />
+                                                        </div>
+
+                                                        {/* <ul className="bg-white w-[290px] hidden group-hover:flex -ml-20 mt-[18px] rounded-lg text-richblack-900 absolute  flex-col gap-5 p-8">
+                                                            <li className="hover:bg-richblack-50 rounded-lg pl-4 py-2 pr-16 text-left">Python </li>
+                                                            <li className="hover:bg-richblack-50 rounded-lg pl-4 py-2 pr-16 text-left">Web devlopment</li>
+                                                            <li className="hover:bg-richblack-50 rounded-lg pl-4 py-2 pr-16 text-left">Linux</li>
+                                                            </ul> 
+                                                        */}
 
 
-                                            
-                                                </div>
-                                            ) :
-                                            (
-                                                <Link to={item.path} >
-                                                {item.title}
-                                                </Link>
-                                            )
-                                        }
 
-                                    </li>
-                                )
-                            })
-                        }
+                                                    </div>
+                                                ) :
+                                                (
+                                                    <Link to={item.path} >
+                                                        {item.title}
+                                                    </Link>
+                                                )
+                                            }
 
-                    </ul>
+                                        </li>
+                                    )
+                                })
+                            }
+
+                        </ul>
+                    </nav>
+
 
 
 
                     {/* profile / login-logout */}
 
-                    if(token) {
-
-                    }
 
                     {/* login / signup */}
 
