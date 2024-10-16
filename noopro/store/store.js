eb5944f4bebcf0a769e5e0990f33ec53a884cd19
Aman Burnwal <amankr06534@gmail.com>
2024-10-16 18:20:14 +0530

    redux store

---
+++ noopro/store/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+import authSlice from "./slice/authSlice"
+import profileSlice from "./slice/profileSlice"
+import cartSlice from "./slice/cartSlice";
+const store = configureStore({
+    reducer: {
+        auth: authSlice,
+        // profile: profileSlice,
+        // cart: cartSlice,
+    }
+})
+
+export default store;
