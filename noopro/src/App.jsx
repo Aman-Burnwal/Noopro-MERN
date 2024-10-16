bcf3f64a4d1751a3d5fabd539710cb6e6b24c889
Aman Burnwal <amankr06534@gmail.com>
2024-10-16 18:23:19 +0530

    store initilization

--- noopro/src/App.jsx
+++ noopro/src/App.jsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import { Navigation } from "./utils"
+
 
 
 const App = () => {
   return (
     <div className="">
+        <Navigation />
 
-    <Routes >
-      <Route path="/" element={<Home/>}/>
-    </Routes>
+      <Routes >
+        <Route path="/" element={<Home/>}/>
+        </Routes>
+      
       
     </div>
   )
--- noopro/src/main.jsx
+++ noopro/src/main.jsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from "react-redux"
 import App from './App.jsx'
 import './index.css'
+import { store } from './utils/index.js'
+import {Toaster} from "react-hot-toast"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter >
-      <App />
-    </BrowserRouter>
+    <Provider store={store} >
+      <BrowserRouter >
+          <App />
+          <Toaster />
+      </BrowserRouter>
+    </Provider>
 
   </StrictMode>,
 )
