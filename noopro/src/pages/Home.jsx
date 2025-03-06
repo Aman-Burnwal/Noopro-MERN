f98c1c4618f50a231c6b625691ad797edb02cb87
Aman Burnwal <gpkabk@gmail.com>
2025-03-07 00:16:38 +0530

    Refactor instructor sign-up button layout for improved structure

--- noopro/src/pages/Home.jsx
+++ noopro/src/pages/Home.jsx
@@ -16,22 +16,24 @@ const Home = () => {
                     <div className="w-8/12 mx-auto  pt-1 flex  flex-col justify-center gap-10  p-1 mt-10 text-white">
 
                         {/* instructor sign up button */}
-                        <Link to={"/signUp"} >
+                        <div>
+                            <Link to={"/signUp"} >
 
-                            <div className="group mx-auto mt-16 w-fit rounded-full bg-richblack-800 
+                                <div className="group mx-auto mt-16 w-fit rounded-full bg-richblack-800 
                                         p-1 font-bold text-richblack-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)]
                                         transition-all duration-200 hover:scale-95 hover:drop-shadow-none">
 
-                                <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] 
+                                    <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] 
                                         transition-all duration-200 group-hover:bg-richblack-900">
-                                    <p>Become an Instructor</p>
-                                    <FaArrowRightLong className=" mt-1" />
+                                        <p>Become an Instructor</p>
+                                        <FaArrowRightLong className=" mt-1" />
+
+                                    </div>
+
 
                                 </div>
-
-
-                            </div>
-                        </Link>
+                            </Link>
+                        </div>
 
 
 
