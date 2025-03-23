0d3548832e395820e5548e88d0d4ae582e9a9ae3
Aman Burnwal <gpkabk@gmail.com>
2025-03-23 23:22:16 +0530

    Add token validation and navigation in MyProfile component

--- noopro/src/components/core/Dashboard/MyProfile.jsx
+++ noopro/src/components/core/Dashboard/MyProfile.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+
 
 const MyProfile = () => {
+
+  const navigate = useNavigate()
+
+  const user = useSelector((state) => state.profile.user)
+  const token = useSelector(state => state.auth.token)
+
+ 
+  
+
+
+  useEffect(() => {
+    if(!token) navigate("/")
+  },[token])
+
   return (
     <div>
       
