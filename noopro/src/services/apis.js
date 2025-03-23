02e15bce061eaac0dca37c0e6d169555023823c4
Aman Burnwal <gpkabk@gmail.com>
2025-03-23 23:21:26 +0530

    Add GetUserDetails API endpoint for user profile retrieval

--- noopro/src/services/apis.js
+++ noopro/src/services/apis.js
@@ -5,4 +5,5 @@ export const categories = {
 export const SendOtpAPI = import.meta.env.VITE_BASE_URL + "/auth/sendotp";
 export const SignUpAPI = import.meta.env.VITE_BASE_URL + "/auth/signup";
 export const LoginAPI = import.meta.env.VITE_BASE_URL + "/auth/login";
+export const GetUserDetails = import.meta.env.VITE_BASE_URL + "/profile/getUserDetails"
 export const ResetPasswordTokenAPI = import.meta.env.VITE_BASE_URL + "/auth/reset-password-token";
