ff288fe019f7bbfde68f94caa0f28376822fe0ad
Aman Burnwal <gpkabk@gmail.com>
2025-03-26 16:06:19 +0530

    Add Error component for handling 404 page not found scenario

---
+++ noopro/src/pages/common/Error.jsx
@@ -0,0 +1,14 @@
+
+
+const Error = () => {
+  return (
+    <div className=" w-full h-screen flex flex-col justify-center items-center bg-richblack-800 text-white">
+      <h1 className=" text-5xl font-bold">404</h1>
+      <p className=" text-lg font-semibold">Page Not Found</p>
+      <p className=" text-base font-normal">The page you are looking for does not exist.</p>
+      
+    </div>
+  )
+}
+
+export default Error
