9955ff590c1c962aee286498ef75d5b2218147e6
Aman Burnwal <gpkabk@gmail.com>
2025-03-26 16:03:51 +0530

    Implement protected route for user profile and add dashboard component

--- noopro/src/App.jsx
+++ noopro/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
-import { About, ContactUs, Login, MyProfile, Navigation, ResetPassword, SignUp } from "./utils"
+import { About, ContactUs, Dashboard, Error, Login, MyProfile, Navigation, ProtectedRoute, ResetPassword, SignUp } from "./utils"
 
 
 
@@ -18,7 +18,17 @@ const App = () => {
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<ContactUs />} />
-        <Route path="/dashboard/user-profile" element= {<MyProfile />} />
+        <Route element={<ProtectedRoute />}> 
+          <Route  element={<Dashboard />}>
+            <Route path="/dashboard/my-profile" element={<MyProfile />} />
+          </Route>
+        </Route>
+        
+
+
+        {/* <Route path="*" element={<Error/> } /> */}
+        
+        
       </Routes>
       
       
