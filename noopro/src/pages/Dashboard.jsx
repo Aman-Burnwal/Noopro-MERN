dfc58fdc986b67d962d31873cdbb17e499a4c709
Aman Burnwal <gpkabk@gmail.com>
2025-03-26 16:05:41 +0530

    Integrate Sidebar component into Dashboard for improved layout and navigation

--- noopro/src/pages/Dashboard.jsx
+++ noopro/src/pages/Dashboard.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+
+import { Outlet } from 'react-router-dom'
+import {  Sidebar } from '../utils'
+
+
 
 const Dashboard = () => {
+
+
+
+  
   return (
-    <div>
+
+    <div className=" min-h-screen bg-richblack-800 w-full">
+      <div className=' flex flex-col gap-6 w-fit'>
+          <div className=' '>
+            <Sidebar/>
+          </div>
+          <div className=' '>
+            <Outlet />
+          </div>
+      </div>
       
     </div>
+
   )
 }
 
