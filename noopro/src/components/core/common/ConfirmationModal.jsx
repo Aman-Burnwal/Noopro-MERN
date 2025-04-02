81cf4bef019acc346fd85da93d3af60160990c47
Aman Burnwal <gpkabk@gmail.com>
2025-04-02 13:11:44 +0530

    Add ConfirmationModal component for user confirmation dialogs

---
+++ noopro/src/components/core/common/ConfirmationModal.jsx
@@ -0,0 +1,15 @@
+
+
+const ConfirmationModal = ({text}) => {
+  return (
+    <div className="absolute left-0 top-0 w-screen h-screen bg-richblue-100 bg-opacity-15 ">
+       <div className=" w-full h-full">
+            <div className="flex flex-col">
+
+            </div>
+       </div>
+    </div>
+  )
+}
+
+export default ConfirmationModal
