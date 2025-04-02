21963b8e243d324149c3160b00cf8d0017a8fa92
Aman Burnwal <gpkabk@gmail.com>
2025-04-02 13:44:23 +0530

    Update profileSlice to store user data instead of token in localStorage

--- noopro/store/slice/profileSlice.js
+++ noopro/store/slice/profileSlice.js
@@ -4,17 +4,17 @@ const profileSlice = createSlice({
     name: "profile",
 
     initialState: {
-        user: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : null,
+        user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null,
     },
     reducers: {
 
         setUser : (state, action) => {
-            localStorage.setItem("token", JSON.stringify(action.payload));
+            localStorage.setItem("user", JSON.stringify(action.payload))
             setUser(state.user = action.payload)
           
         },
         removeUser: (state) => {
-        
+            localStorage.removeItem("token");
             setUser(state.user = null)
         },
     }
