c07ec0d323d7900bda8959cb30ba248b934fde04
Aman Burnwal <gpkabk@gmail.com>
2025-04-08 00:10:09 +0530

    Add API endpoints for updating and deleting user profiles

--- noopro/src/services/apis.js
+++ noopro/src/services/apis.js
@@ -9,3 +9,5 @@ export const GetUserDetails = import.meta.env.VITE_BASE_URL + "/profile/getUserD
 export const ResetPasswordTokenAPI = import.meta.env.VITE_BASE_URL + "/auth/reset-password-token";
 export const ProfilePictureAPI =import.meta.env.VITE_BASE_URL + "/profile/updateDisplayPicture"
 export const ChangePasswordAPI = import.meta.env.VITE_BASE_URL + "/auth/changepassword";
+export const UPDATE_PROFILE_API = import.meta.env.VITE_BASE_URL + "/profile/updateProfile"
+export const DELETE_PROFILE_API = import.meta.env.VITE_BASE_URL + "/profile/deleteProfile"
