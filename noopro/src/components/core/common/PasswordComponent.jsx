d5cdf754f8ff5dc4bb1a45a8b0892d666ff09570
Aman Burnwal <gpkabk@gmail.com>
2025-04-07 09:55:45 +0530

    Enhance PasswordComponent to include placeholder and id props, and toggle visibility icon

--- noopro/src/components/core/common/PasswordComponent.jsx
+++ noopro/src/components/core/common/PasswordComponent.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
-import { BiShow } from "react-icons/bi"
+import { BiHide, BiShow } from "react-icons/bi"
 
 
-const PasswordComponent = ({handler, password , text }) => {
+const PasswordComponent = ({setPssword, password , text , placeHolder, id}) => {
 
   const [type, setType] = useState("password");
   const displayHandler = () => {
@@ -11,19 +11,25 @@ const PasswordComponent = ({handler, password , text }) => {
   }
   return (
     <div 
-     onChange={() => handler()}
-    className=" relative flex flex-col gap-2 text-caribbeangreen-900 w-fit ">
-      <label htmlFor="password" >{text}</label>
+     
+    className=" relative flex flex-col gap-2 text-caribbeangreen-900 w-fit  ">
+      <label 
+        className="  text-richblack-25  "
+       htmlFor={id} >{text}</label>
       
       <input
         
-        className="relative" 
-        type={type}
+        className=" relative rounded-lg w-fit bg-richblue-25 pl-3 py-3
+         placeholder:text-richblue-200 placeholder:text-sm " 
+         type={type}
          value={password}
          autoComplete={text}
+         placeholder={placeHolder}
+         onChange={(e) => setPssword(e.target.value)}
+         id={id}
         >
         </input>
-        <span className=" absolute right-3 top-9 " onClick={() => displayHandler()}> <BiShow /> </span>
+        <span className=" absolute right-3 top-12 cursor-pointer " onClick={() => displayHandler()}>{type == "password" ?  <BiShow className=" " /> : <BiHide />} </span>
     </div>
   )
 }
