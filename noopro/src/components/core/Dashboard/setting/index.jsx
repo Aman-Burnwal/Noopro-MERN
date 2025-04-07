ebd07ac1fb82540c52f49cf4e1e7a15c16644256
Aman Burnwal <gpkabk@gmail.com>
2025-04-07 09:56:51 +0530

    Add Settings component to manage user profile features

---
+++ noopro/src/components/core/Dashboard/setting/index.jsx
@@ -0,0 +1,24 @@
+import ChangeProfilePicture from "./ChangeProfilePicture"
+import DeleteAccount from "./DeleteAccount"
+import EditProfile from "./EditProfile"
+import UpdatePassword from "./UpdatePassword"
+
+export default function Settings() {
+  return (
+    <div className=" w-full">
+    <div className=" w-9/12 p-14 mx-auto">
+      <h1 className="mb-14 text-3xl font-medium text-richblack-5">
+        Edit Profile
+      </h1>
+      {/* Change Profile Picture */}
+      <ChangeProfilePicture />
+      {/* Profile */}
+      <EditProfile />
+      {/* Password */}
+      <UpdatePassword />
+      {/* Delete Account */}
+      <DeleteAccount />
+      </div>
+    </div>
+  )
+}
