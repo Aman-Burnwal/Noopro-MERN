6dd65c41ab2c3377d1b29d03f256cb8810968dd7
Aman Burnwal <gpkabk@gmail.com>
2025-04-08 12:17:38 +0530

    Fix typos in User model schema and improve formatting for better readability

--- noopro/server/model/User.js
+++ noopro/server/model/User.js
@@ -1,60 +1,63 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
-    }, 
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    password: {
-        type: String,
-        required: true,
-      
-    },
-    accountType: {
-        type: String,
-        required: true,
-        enum: ["Admin", "Student", "Instructor"]
-    },
-    additonDetail: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "Profile"
-    },
-    courses: [
-        {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Course"
-        }
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
 
-    ],
-    token: {
-        type: String
-    },
-    resetPasswordExpires: {
-        tyep: Date
-    },
-    iamge: {
-        type: String,
-        required: true
-    },
-    courseProgess: [
-        {
+        },
+        accountType: {
+            type: String,
+            required: true,
+            enum: ["Admin", "Student", "Instructor"]
+        },
+        additonDetail: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "CourseProgress"
-        }
-    ]
+            required: true,
+            ref: "Profile"
+        },
+        courses: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Course"
+            }
 
-})
+        ],
+        token: {
+            type: String
+        },
+        resetPasswordExpires: {
+            tyep: Date
+        },
+        iamge: {
+            type: String,
+            required: true
+        },
+        courseProgess: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "CourseProgress"
+            }
+        ]
+
+    },
+    { timestamps: true }
+)
 
 module.exports = mongoose.model("User", UserSchema);
