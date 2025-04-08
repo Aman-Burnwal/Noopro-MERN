41a332f22395800bb9a0b383dd5a12f71830780d
Aman Burnwal <gpkabk@gmail.com>
2025-04-08 21:39:34 +0530

    Add TipsForAddingCourse component with upload tips for course creation

---
+++ noopro/src/components/core/Dashboard/AddCourse/Components/TipsForAddingCourse.jsx
@@ -0,0 +1,24 @@
+
+
+const TipsForAddingCourse = () => {
+  return (
+    <div className="w-fit bg-richblue-900">
+      <div className="p-5  text-richblack-25">
+          <h2 className=" py-2"><span>⚡</span> Course Upload Tips </h2>
+
+          <oll className=" text-xs leading-5">
+            <li>Set the Course Price option or make it free.</li>
+            <li>Standard size for the course thumbnail is 1024x576.</li>
+            <li>Video section controls the course overview video.</li>
+            <li>Course Builder is where you create & organize a course.</li>
+            <li>Add Topics in the Course Builder section to create lessons, quizzes, and assignments.</li>
+            <li>Information from the Additional Data section shows up on the course single page.</li>
+            <li>Make Announcements to notify any important</li>
+            <li>Notes to all enrolled students at once.</li>
+          </oll>
+      </div>
+    </div>
+  )
+}
+
+export default TipsForAddingCourse
