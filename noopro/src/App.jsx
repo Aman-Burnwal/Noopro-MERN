fdba03b54f939e2c9e103f6d5497bbfc896d1726
Aman Burnwal <gpkabk@gmail.com>
2025-04-08 13:25:59 +0530

    Refactor routing in App.jsx to improve structure and add Cart route for students

--- noopro/src/App.jsx
+++ noopro/src/App.jsx
@@ -1,40 +1,60 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
-import { About, ContactUs, Dashboard, EditProfile, Error, Login, MyProfile, Navigation, ProtectedRoute, ResetPassword, Settings, SignUp } from "./utils"
+import { About, Cart, ContactUs, Dashboard, EnrolledCourses, Error, Login, MyProfile, Navigation, ProtectedRoute, ResetPassword, Settings, SignUp } from "./utils"
+import { useSelector } from "react-redux"
+import { ACCOUNT_TYPE } from "./utils/constants"
 
 
 
 
 
 const App = () => {
+
+  const user = useSelector((store) => store.profile.user);
+
   return (
     <div className="">
-        <Navigation />
+      <Navigation />
 
       <Routes >
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login"  element={<Login/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<ContactUs />} />
-        <Route element={<ProtectedRoute />}> 
-          <Route  element={<Dashboard />}>
+        <Route element={<ProtectedRoute />}>
+          <Route element={<Dashboard />}>
             <Route path="/dashboard/my-profile" element={<MyProfile />} />
-            <Route path="/dashboard/settings" element ={<Settings />} />
+            <Route path="/dashboard/settings" element={<Settings />} />
+            {
+              user?.accountType === ACCOUNT_TYPE.STUDENT && (
+                <>
+                  <Route path="dashboard/cart" element={<Cart />} />
+                  <Route path="/dashboard/enrolled-courses" element={<EnrolledCourses />} />
+                </>
+              )
+            }
+
+            {
+              user.ACCOUNT_TYPE == ACCOUNT_TYPE.INSTRUCTOR && (
+                <Route path="/dashboard/enrolled-courses" element={<EnrolledCourses />} />
+              )
+            }
+            
 
           </Route>
         </Route>
-        
 
 
-        <Route path="*" element={<Error/> } />
-        
-        
+
+        <Route path="*" element={<Error />} />
+
+
       </Routes>
-      
-      
+
+
     </div>
   )
 }
