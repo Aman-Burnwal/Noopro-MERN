568e0f85d443480ed5909f1443a9a4c8b7bef082
Aman Burnwal <gpkabk@gmail.com>
2025-04-22 14:18:49 +0530

    Add editCourse handler for updating course details and thumbnail image

--- noopro/server/controllers/Course.js
+++ noopro/server/controllers/Course.js
@@ -65,12 +65,23 @@ exports.createCourse = async (req, res) => {
         )
 
         // update the tag ka schema 
+        const categoryDetails2 = await Category.findByIdAndUpdate(
+            { _id: category },
+            {
+              $push: {
+                courses: nayaCourse._id,
+              },
+            },
+            { new: true }
+          )
+          console.log("HEREEEEEEEE", categoryDetails2)
         // todo HW
         // return response
 
         return res.status(200).json({
             success: true,
-            message: "course created successfully"
+            message: "course created successfully",
+            data: nayaCourse
         })
 
 
@@ -84,6 +95,71 @@ exports.createCourse = async (req, res) => {
     }
 }
 
+
+exports.editCourse = async (req, res) => {
+    try {
+      const { courseId } = req.body
+      const updates = req.body
+      const course = await Course.findById(courseId)
+  
+      if (!course) {
+        return res.status(404).json({ error: "Course not found" })
+      }
+  
+      // If Thumbnail Image is found, update it
+      if (req.files) {
+        console.log("thumbnail update")
+        const thumbnail = req.files.thumbnailImage
+        const thumbnailImage =  await uploadImageToCloudinay(thumbnail, process.env.FOLDER_NAME)
+        course.thumbnail = thumbnailImage.secure_url
+      }
+  
+      // Update only the fields that are present in the request body
+      for (const key in updates) {
+        if (updates.hasOwnProperty(key)) {
+          if (key === "tag" || key === "instructions") {
+            course[key] = JSON.parse(updates[key])
+          } else {
+            course[key] = updates[key]
+          }
+        }
+      }
+  
+      await course.save()
+  
+      const updatedCourse = await Course.findOne({
+        _id: courseId,
+      })
+        .populate({
+          path: "instructor",
+          populate: {
+            path: "additonDetail",
+          },
+        })
+        .populate("category")
+        .populate("ratingAndReviews")
+        .populate({
+          path: "courseContent",
+          populate: {
+            path: "SubSection",
+          },
+        })
+        .exec()
+  
+      res.json({
+        success: true,
+        message: "Course updated successfully",
+        data: updatedCourse,
+      })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({
+        success: false,
+        message: "Internal server error",
+        error: error.message,
+      })
+    }
+  }
 
 //get all course
 
