fb196806f220ea2cef77df788b430a55227b32d6
Aman Burnwal <gpkabk@gmail.com>
2025-04-22 13:57:46 +0530

    Add courseSlice to the Redux store configuration

--- noopro/store/store.js
+++ noopro/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import authSlice from "./slice/authSlice"
 import profileSlice from "./slice/profileSlice"
 import cartSlice from "./slice/cartSlice";
+import courseSlice from "./slice/courseSlice"
 const store = configureStore({
     reducer: {
         auth: authSlice,
         profile: profileSlice,
         cart: cartSlice,
+        course: courseSlice,
     }
 })
 
