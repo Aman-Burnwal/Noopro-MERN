071f623d341516d67170811778be5a2085babec0
Aman Burnwal <gpkabk@gmail.com>
2025-04-22 14:00:56 +0530

    Uncomment editCourse route to enable course editing functionality

--- noopro/server/routers/Course.js
+++ noopro/server/routers/Course.js
@@ -11,7 +11,7 @@ const {
   getCourseDetails,
   // getFullCourseDetails,
   
-//   editCourse,
+  editCourse,
 //   getInstructorCourses,
 //   deleteCourse,
 } = require("../controllers/Course")
@@ -78,7 +78,7 @@ router.post("/getCourseDetails", getCourseDetails)
 // Get Details for a Specific Courses
 // router.post("/getFullCourseDetails", auth, getFullCourseDetails)
 // Edit Course routes
-// router.post("/editCourse", auth, isInstructor, editCourse)
+router.post("/editCourse", auth, isInstructor, editCourse)
 // Get all Courses Under a Specific Instructor
 // router.get("/getInstructorCourses", auth, isInstructor, getInstructorCourses)
 // Delete a Course
