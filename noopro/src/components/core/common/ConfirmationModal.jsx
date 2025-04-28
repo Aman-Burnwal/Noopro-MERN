1dc27bb72cdc0bac15e755f337104024fa27cfdf
Aman Burnwal <gpkabk@gmail.com>
2025-04-28 11:38:06 +0530

    Refactor ConfirmationModal component to use modalData for dynamic content rendering and improve layout structure

--- noopro/src/components/core/common/ConfirmationModal.jsx
+++ noopro/src/components/core/common/ConfirmationModal.jsx
@@ -1,15 +1,28 @@
+import IconBtn from "./IconBtn"
 
-
-const ConfirmationModal = ({text}) => {
+export default function ConfirmationModal({ modalData }) {
   return (
-    <div className="absolute left-0 top-0 w-screen h-screen bg-richblue-100 bg-opacity-15 ">
-       <div className=" w-full h-full">
-            <div className="flex flex-col">
-
-            </div>
-       </div>
+    <div className="fixed inset-0 z-[1000] !mt-0 grid place-items-center overflow-auto bg-white bg-opacity-10 backdrop-blur-sm">
+      <div className="w-11/12 max-w-[350px] rounded-lg border border-richblack-400 bg-richblack-800 p-6">
+        <p className="text-2xl font-semibold text-richblack-5">
+          {modalData?.text1}
+        </p>
+        <p className="mt-3 mb-5 leading-6 text-richblack-200">
+          {modalData?.text2}
+        </p>
+        <div className="flex items-center gap-x-4">
+          <IconBtn
+            onclick={modalData?.btn1Handler}
+            text={modalData?.btn1Text}
+          />
+          <button
+            className="cursor-pointer rounded-md bg-richblack-200 py-[8px] px-[20px] font-semibold text-richblack-900"
+            onClick={modalData?.btn2Handler}
+          >
+            {modalData?.btn2Text}
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
-
-export default ConfirmationModal
