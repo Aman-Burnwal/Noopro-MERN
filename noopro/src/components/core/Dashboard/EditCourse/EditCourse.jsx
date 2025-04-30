ae6b9306e6a4fb5549493fffba2bd4139fb8c7de
Aman Burnwal <gpkabk@gmail.com>
2025-04-30 15:27:16 +0530

    Refactor EditCourse component: implement loading state and fetch course details on mount

--- noopro/src/components/core/Dashboard/EditCourse/EditCourse.jsx
+++ noopro/src/components/core/Dashboard/EditCourse/EditCourse.jsx
@@ -1,11 +1,55 @@
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useParams } from "react-router-dom"
+
+import StepRender from "../AddCourse/StepRender"
+import { setCourse, setEditCourse } from "../../../../../store/slice/courseSlice"
+import { getFullDetailsOfCourse } from "../../../../hook/instructors"
 
 
-const EditCourse = () => {
+export default function EditCourse() {
+  const dispatch = useDispatch()
+  const { courseId } = useParams()
+  const { course } = useSelector((state) => state.course)
+  const [loading, setLoading] = useState(false)
+  const { token } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    ;(async () => {
+      setLoading(true)
+      const result = await getFullDetailsOfCourse(courseId, token , dispatch)
+      console.log(result)
+      if (result) {
+        dispatch(setEditCourse(true))
+        dispatch(setCourse(result))
+      }
+      setLoading(false)
+    })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="grid flex-1 place-items-center">
+        <div className="spinner"></div>
+      </div>
+    )
+  }
+
   return (
     <div>
-      
+      <h1 className="mb-14 text-3xl font-medium text-richblack-5">
+        Edit Course
+      </h1>
+      <div className="mx-auto max-w-[600px]">
+        {course ? (
+          <StepRender />
+        ) : (
+          <p className="mt-14 text-center text-3xl font-semibold text-richblack-100">
+            Course not found
+          </p>
+        )}
+      </div>
     </div>
   )
 }
-
-export default EditCourse
