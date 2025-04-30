1438e2c04c81ad3d364a625f95112c5421e3e569
Aman Burnwal <gpkabk@gmail.com>
2025-04-30 15:26:22 +0530

    Uncomment getFullCourseDetails and deleteCourse routes in Course.js

--- noopro/server/routers/Course.js
+++ noopro/server/routers/Course.js
@@ -9,11 +9,11 @@ const {
   createCourse,
   showAllCourses,
   getCourseDetails,
-  // getFullCourseDetails,
+  getFullCourseDetails,
   
   editCourse,
   getInstructorCourses,
-//   deleteCourse,
+  deleteCourse,
 } = require("../controllers/Course")
 
 // const {
@@ -76,13 +76,13 @@ router.get("/getAllCourses", showAllCourses)
 // Get Details for a Specific Courses
 router.post("/getCourseDetails", getCourseDetails)
 // Get Details for a Specific Courses
-// router.post("/getFullCourseDetails", auth, getFullCourseDetails)
+router.post("/getFullCourseDetails", auth, getFullCourseDetails)
 // Edit Course routes
 router.post("/editCourse", auth, isInstructor, editCourse)
 // Get all Courses Under a Specific Instructor
 router.get("/getInstructorCourses", auth, isInstructor, getInstructorCourses)
 // Delete a Course
-// router.delete("/deleteCourse", deleteCourse)
+router.delete("/deleteCourse", deleteCourse)
 // router.post("/updateCourseProgress", auth, isStudent, updateCourseProgress);
 
 // ********************************************************************************************************
