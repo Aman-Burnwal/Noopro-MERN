ef728b7e5979126a75fbd7f009c9f71fc749ba61
Aman Burnwal <gpkabk@gmail.com>
2025-05-04 17:29:18 +0530

    feat: implement fetchCourseDetails function to retrieve course data by courseId

--- noopro/src/hook/Courses/index.js
+++ noopro/src/hook/Courses/index.js
@@ -1,5 +1,5 @@
 import toast from "react-hot-toast"
-import { CATEGORIES_COURSE_API, SHOW_ALL_CATAGORIES_COURSE_API } from "../../services/apis";
+import { CATEGORIES_COURSE_API, GET_COURSE_DETAILS_API, SHOW_ALL_CATAGORIES_COURSE_API } from "../../services/apis";
 import { removeUser } from "../../../store/slice/profileSlice";
 import { removeToken } from "../../../store/slice/authSlice";
 
@@ -72,4 +72,123 @@ export const getAllCategoriesCourse = async (dispatch) => {
 
         toast.dismiss(toastId);
         return response;
+}
+
+export const fetchCourseDetails = async ( courseId) => {
+        // const toastId = toast.loading("Loading...")
+        console.log(courseId)
+        //   dispatch(setLoading(true));
+        let result = null
+        try {
+                await fetch(GET_COURSE_DETAILS_API, {
+                        method: "POST",
+                        headers : {
+                                "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({courseId}),
+                   
+                })
+                .then(res => {
+
+                        // console.log(res);
+                        if(res.status == 400) {
+                                console.log("CourseId is wrong")
+                                return;
+                        }
+
+                        // console.log(res)
+                        
+                        return res.json()
+                })
+                .then(res => {
+                        console.log(res)
+                        result = res.data[0]
+                })
+                .catch(error => {
+                        console.log("error ocuured")
+                        console.log(error)
+                })
+        } catch (error) {
+                result = error
+                console.log(error);
+        }
+
+        // toast.dismiss(toastId);
+        return result;
+}
+
+
+
+function loadScript(src) {
+    return new Promise((resolve) => {
+        const script = document.createElement("script");
+        script.src = src;
+
+        script.onload = () => {
+            resolve(true);
+        }
+        script.onerror= () =>{
+            resolve(false);
+        }
+        document.body.appendChild(script);
+    })
+}
+
+
+export async function buyCourse(token, courses, userDetails, navigate, dispatch) {
+    const toastId = toast.loading("Loading...");
+    try{
+        //load the script
+        const res = await loadScript("https://checkout.razorpay.com/v1/checkout.js");
+
+        if(!res) {
+            toast.error("RazorPay SDK failed to load");
+            return;
+        }
+
+        //initiate the order
+        const orderResponse = await apiConnector("POST", COURSE_PAYMENT_API, 
+                                {courses},
+                                {
+                                    Authorization: `Bearer ${token}`,
+                                })
+
+        if(!orderResponse.data.success) {
+            throw new Error(orderResponse.data.message);
+        }
+        console.log("PRINTING orderResponse", orderResponse);
+        //options
+        const options = {
+            key: process.env.RAZORPAY_KEY,
+            currency: orderResponse.data.message.currency,
+            amount: `${orderResponse.data.message.amount}`,
+            order_id:orderResponse.data.message.id,
+            name:"StudyNotion",
+            description: "Thank You for Purchasing the Course",
+            image:rzpLogo,
+            prefill: {
+                name:`${userDetails.firstName}`,
+                email:userDetails.email
+            },
+            handler: function(response) {
+                //send successful wala mail
+                sendPaymentSuccessEmail(response, orderResponse.data.message.amount,token );
+                //verifyPayment
+                verifyPayment({...response, courses}, token, navigate, dispatch);
+            }
+        }
+        //miss hogya tha 
+        const paymentObject = new window.Razorpay(options);
+        paymentObject.open();
+        paymentObject.on("payment.failed", function(response) {
+            toast.error("oops, payment failed");
+            console.log(response.error);
+        })
+
+    }
+    catch(error) {
+        console.log("PAYMENT API ERROR.....", error);
+        toast.error("Could not make Payment");
+    }
+    toast.dismiss(toastId);
 }
