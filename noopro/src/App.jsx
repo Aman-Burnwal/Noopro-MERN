fdbd1851d2f28bf5790bb025e62c786cbf0b21c7
Aman Burnwal <gpkabk@gmail.com>
2025-05-04 17:29:51 +0530

    feat: add CourseDetails route to App component for course-specific views

--- noopro/src/App.jsx
+++ noopro/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
-import { About, AddCourse, Cart, Catalog, ContactUs, Dashboard, EditCourse, EnrolledCourses, Error, Login, MyCourses, MyProfile, Navigation, ProtectedRoute, ResetPassword, Settings, SignUp } from "./utils"
+import { About, AddCourse, Cart, Catalog, ContactUs, CourseDetails, Dashboard, EditCourse, EnrolledCourses, Error, Login, MyCourses, MyProfile, Navigation, ProtectedRoute, ResetPassword, Settings, SignUp } from "./utils"
 import { useSelector } from "react-redux"
 import { ACCOUNT_TYPE } from "./utils/constants"
 
@@ -21,6 +21,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/catalog/:catalogName" element ={<Catalog />} />
+        <Route path="courses/:courseId" element={<CourseDetails/>} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<ContactUs />} />
